27aeabe7c99ee74910936ee976c2eaf7e9f08fbd
shariful4345 <mdsajeeb4345@gmail.com>
2022-09-28 22:55:49 +0600

    header section added

---
+++ src/component/Headers/Headers.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Headers = () => {
+    return (
+        <div>
+            <div className='text-5xl font-semibold text-red-700 '>
+                <h2><span className=' font-bold  text-emerald-600 '>BD</span> Sport Club.</h2>
+           </div>
+        </div>
+    );
+};
+
+export default Headers;
